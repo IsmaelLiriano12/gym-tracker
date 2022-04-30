33aeaf5e376f39be6123cc44be5c69c62813e9c3
ismael Liriano <ismaelliriano15@gmail.com>
2022-04-30 14:36:33 -0400

    Added ProgressiveOverload class

---
+++ src/GymTracker/GymTrackerShared/Commands/AddProgressiveOverloadCommand.cs
@@ -0,0 +1,37 @@
+﻿using GymTrackerShared.Data;
+using GymTrackerShared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymTrackerShared.Commands
+{
+    public class AddProgressiveOverloadCommand
+    {
+        private readonly Context _context;
+
+        public AddProgressiveOverloadCommand(Context context)
+        {
+            this._context = context;
+        }
+
+        public ProgressiveOverload Execute(int exerciseId, decimal weight, int reps, int sets)
+        {
+            var progress = new ProgressiveOverload()
+            {
+                ExerciseId = exerciseId,
+                DateCreated = DateTime.Now,
+                Weight = weight,
+                Repetitions = reps,
+                Sets = sets
+            };
+
+            _context.ProgressiveOverloads.Add(progress);
+            _context.SaveChanges();
+
+            return progress;
+        }
+    }
+}
